e90dd96c36dc06c2c4be767177ab35f2d1aea7ca
suksmono <38452529+suksmono@users.noreply.github.com>
2019-04-26 14:07:30 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+    name='QSymbolic',
+    version='0.1dev',
+    packages=['qsymbolic',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
