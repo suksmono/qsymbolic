ff5f813ef38e868f062ee704245ec34185ea4868
suksmono <38452529+suksmono@users.noreply.github.com>
2019-05-02 16:19:00 +0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
 setup(
     name='QSymbolic',
-    version='0.1dev',
-    packages=['qsymbolic',],
+    url='https://github.com/suksmono/qsymbolic',
+    author='Andriyan Suksmono',
+    author_email='suksmono@yahoo.com',
+    #install_requires=['sympy'],
+    version='0.2dev',
+    packages=['qsymbolic'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
 )
